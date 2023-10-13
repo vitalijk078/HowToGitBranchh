417d276f7b0b229ece92632c6648ff1cd674c427
Виталий <vitalijk078@gmail.com>
2023-10-13 09:59:18 +0300

    removed task14

--- src/task14.js
+++
@@ -1,6 +0,0 @@
-const name = "John";
-const lastName = "Doe";
-const age = 25;
-console.log("Имя:        " + name);
-console.log("Фамилия:    " + lastName);
-console.log("Возраст:    " + age + " лет");
